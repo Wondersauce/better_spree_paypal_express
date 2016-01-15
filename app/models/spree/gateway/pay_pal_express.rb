40c93b4ea1f199b2fa4c9317900ac2a2627b5f36
vinney cavallo <vcavallo@gmail.com>
2016-01-15 17:01:55 -0500

    Move error handler module

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -1,4 +1,18 @@
 require 'paypal-sdk-merchant'
+
+# hacking this here so that the Spree Refund#process! method doesn't choke.
+# snippet from #process! below:
+# text = response.params['message'] || response.params['response_reason_text'] || response.message
+module PayPalErrorHandling
+  def message
+    self.errors.collect{ |e| e.long_message }.join(",")
+  end
+
+  def params
+    { 'message' => message }
+  end
+end
+
 module Spree
   class Gateway::PayPalExpress < Gateway
     preference :login, :string
